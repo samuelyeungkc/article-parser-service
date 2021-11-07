0823ce813071a1574d79ddf6d616f97259bb697f
Samuel Yeung <samuelyeung.hk@gmail.com>
2021-11-07 02:49:03 -0500

    refactored to function

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const fs = require('fs');
-const crypto = require('crypto');
+function parseArticle(parseUrl, callback) {
+	const { extract } = require('article-parser');
+	extract(parseUrl).then((article) => {
+		//  console.log(article);
+		  const content = article.content;
 
-const {
-  extract
-} = require('article-parser');
+		  const crypto = require('crypto');
+		  const hash = crypto.createHash('md5').update(parseUrl).digest('hex');
+		  const writeFile = `${__dirname}/${hash}.article`;
+		  console.log(writeFile);
+		  const fs = require('fs');
+		  fs.writeFileSync(writeFile, JSON.stringify(article));
 
-const url = 'https://www.zdnet.com/article/dumping-google-chrome-heres-the-best-browser-to-replace-it/?sdfdsf=dsda';
+		  if (callback && article.content) {
+			  callback(article.content);
+		  }
+		}).catch((err) => {
+		  console.log(err);
+		});
 
-extract(url).then((article) => {
-//  console.log(article);
-  const content = article.content;
+}
 
-  const hash = crypto.createHash('md5').update(url).digest('hex');
-  const writeFile = `${__dirname}/${hash}`;
-  console.log(writeFile);
-  fs.writeFileSync(writeFile, JSON.stringify(article));
-}).catch((err) => {
-  console.log(err);
-});
 
+const express = require('express');
+const app = express();
+const port = 8989;
+
+app.get('/', (req, res) => {
+	console.log(req.query);
+	parseArticle(
+		'https://zacharydcarter.substack.com/p/what-the-virginia-election-means', 
+		(content) => {
+			res.send(content);
+		}
+	);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
